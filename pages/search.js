3d936c38fc33b52c5dd8deeb9b98c851fc59a60a
Mahesh <mahipara2021@gmail.com>
2022-02-23 17:18:51 +0530

    API Active

--- pages/search.js
+++ pages/search.js
@@ -26,7 +26,7 @@ export default Search
 export async function getServerSideProps(context){
     const API_KEY = process.env.API_KEY
     const CONTEXT_KEY = process.env.CONTEXT_KEY
-    const useDummyData=true;
+    const useDummyData=false;
     const startIndex = context.query.start || "0"
 
     const data = useDummyData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`).then(res=>res.json())
